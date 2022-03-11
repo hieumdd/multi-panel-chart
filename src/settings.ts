4d807e93f49edc335be0eda391fa411ea776a76e
HM <hieumdd@gmail.com>
2022-03-12 00:20:50 +0700

    feat: Switched to table dataView, added data bound enums

--- src/settings.ts
+++ src/settings.ts
@@ -3,18 +3,15 @@
 import { dataViewObjectsParser } from 'powerbi-visuals-utils-dataviewutils';
 import DataViewObjectsParser = dataViewObjectsParser.DataViewObjectsParser;
 
-export class PanelSettings {
-    public left1Color: string = '#991b1b';
-    public left1Area: boolean = false;
-    public left2Color: string = '#991b1b';
-    public left2Area: boolean = false;
-    public right1Color: string = '#991b1b';
-    public right1Area: boolean = false;
-    public right2Color: string = '#991b1b';
-    public right2Area: boolean = false;
+export class ColorSettings {
+    public color: string = '#991b1b';
+}
+
+export class AreaSettings {
+    public area: boolean = false;
 }
 
 export class VisualSettings extends DataViewObjectsParser {
-    public panel1: PanelSettings = new PanelSettings();
-    public panel2: PanelSettings = new PanelSettings();
+    public color: ColorSettings = new ColorSettings();
+    public area: AreaSettings = new AreaSettings();
 }
--- src/visual.ts
+++ src/visual.ts
@@ -5,99 +5,99 @@ import powerbi from 'powerbi-visuals-api';
 import VisualConstructorOptions = powerbi.extensibility.visual.VisualConstructorOptions;
 import VisualUpdateOptions = powerbi.extensibility.visual.VisualUpdateOptions;
 import IVisual = powerbi.extensibility.visual.IVisual;
+import IVisualHost = powerbi.extensibility.visual.IVisualHost;
 import DataView = powerbi.DataView;
+import DataViewObjects = powerbi.DataViewObjects;
+import DataViewMetadata = powerbi.DataViewMetadata;
 
 // Formatting Options
+import VisualObjectInstance = powerbi.VisualObjectInstance;
 import EnumerateVisualObjectInstancesOptions = powerbi.EnumerateVisualObjectInstancesOptions;
 import VisualObjectInstanceEnumeration = powerbi.VisualObjectInstanceEnumeration;
+import VisualEnumerationInstanceKinds = powerbi.VisualEnumerationInstanceKinds;
 import { VisualSettings } from './settings';
+
+import { ColorHelper } from 'powerbi-visuals-utils-colorutils';
 
 import { groupBy, zip, flattenDepth, min, max, round } from 'lodash-es';
 import * as echarts from 'echarts';
 
 import tooltip from './components/tooltip';
-import legend from './components/legend'
-import axisPointer from './components/axisPointer'
+import legend from './components/legend';
+import axisPointer from './components/axisPointer';
+import { dataViewObjects } from 'powerbi-visuals-utils-dataviewutils';
 
 type Data = {
     id: string;
+    panelId: string;
+    yAxisId: string;
+    seriesIndexId: string;
     date: Date;
-    panel: string;
-    yAxisAlign: string;
     key: string;
     value: number;
-};
-
-const panelMap = {
-    1: 'panel1',
-    2: 'panel2',
-};
-
-const panelOptionsMap = {
-    color: {
-        left: {
-            1: 'left1Color',
-            2: 'left2Color',
-        },
-        right: {
-            1: 'right1Color',
-            2: 'right2Color',
-        },
-    },
-    isArea: {
-        left: {
-            1: 'left1Area',
-            2: 'left2Area',
-        },
-        right: {
-            1: 'right1Area',
-            2: 'right2Area',
-        },
-    },
+    color: string;
 };
 
 const chain = '-';
 
+const getColor = (objects: DataViewObjects) =>
+    objects
+        ? dataViewObjects.getFillColor(objects, {
+              objectName: 'color',
+              propertyName: 'color',
+          })
+        : '#000000';
+
 const mapDataView = (dataView: DataView): Data[] => {
-    const { categories = [], values = [] } = dataView.categorical;
+    const { columns, rows } = dataView.table;
 
-    const columnsData = [...categories, ...values].map(
-        ({ source: { roles, displayName }, values }) => [
-            Object.keys(roles)[0],
-            displayName,
-            values,
-        ],
-    );
+    const dateValues = <string[]>rows.map((row) => row[0]);
 
-    const dateColumn = columnsData[0];
-    const dateValues = dateColumn[2];
-    const dataColumn: Data[][] = columnsData
+    const dataObjects = dataView.metadata.columns
         .slice(1)
-        .map(([id, key, data]: [id: string, key: string, data: number[]]) =>
-            zip<Date, number>(dateValues, data).map(([date, value]) => {
-                const [panel, yAxisAlign, _] = id.split('-');
-                return { id, date, panel, yAxisAlign, key, value };
-            }),
+        .map(({ objects }) => objects);
+
+    const dataValues = rows
+        .map((row) => <number[]>row.slice(1))
+        .map((row) =>
+            zip(
+                columns
+                    .slice(1)
+                    .map((column) => [
+                        Object.keys(column.roles)[0],
+                        column.displayName,
+                    ]),
+                row,
+            ).map(([[id, key], value]) => ({ id, key, value })),
         );
-    return flattenDepth<Data>(dataColumn, 1);
+
+    const matchedData = zip(dateValues, dataValues).map(([date, values]) => {
+        return zip(values, dataObjects).map(([value, object]) => ({
+            ...value,
+            date: new Date(date),
+            color: getColor(object),
+        }));
+    });
+
+    return flattenDepth(matchedData, 1).map((d) => {
+        const [panelId, yAxisId, seriesIndexId] = d.id.split(chain);
+        return {
+            ...d,
+            panelId,
+            yAxisId,
+            seriesIndexId,
+        };
+    });
 };
 
-const buildOptions = (data: Data[], settings: VisualSettings) => {
-    const getSettingsMap = (
-        key: string,
-        [panelId, yAxisAlign, seriesAxisId]: string[],
-    ): string =>
-        settings[panelMap[panelId]][
-            panelOptionsMap[key][yAxisAlign][seriesAxisId]
-        ];
-
+const buildOptions = (data: Data[]) => {
     const groupData = (fn: (d: Data) => string | number) => groupBy(data, fn);
-    const panelData = groupData(({ panel }) => panel);
-    const axisData = groupData(({ panel, yAxisAlign }) =>
-        [panel, yAxisAlign].join(chain),
+    const panelData = groupData(({ panelId }) => panelId);
+    const axisData = groupData(({ panelId, yAxisId }) =>
+        [panelId, yAxisId].join(chain),
     );
-    const seriesData = groupData(({ panel, yAxisAlign, key }) =>
-        [panel, yAxisAlign, key].join(chain),
+    const seriesData = groupData(({ panelId, yAxisId, key }) =>
+        [panelId, yAxisId, key].join(chain),
     );
 
     const grid = Object.entries(panelData).map(([id], i, arr) => ({
@@ -134,10 +134,7 @@ const buildOptions = (data: Data[], settings: VisualSettings) => {
 
     const series = Object.entries(seriesData).map(([id, seriesData]) => {
         const [xAxisId, yAxis, key] = id.split(chain);
-        const seriesIds = seriesData
-            .reduce((_, cur) => cur.id, '')
-            .split(chain);
-        const color = getSettingsMap('color', seriesIds);
+        const color = seriesData.reduce((_, cur) => cur.color, '');
         return {
             type: 'line',
             name: `${xAxisId}-${key}`,
@@ -147,7 +144,7 @@ const buildOptions = (data: Data[], settings: VisualSettings) => {
             data: seriesData.map(({ date, value }) => [date, value]),
             lineStyle: { color },
             itemStyle: { color },
-            areaStyle: getSettingsMap('isArea', seriesIds) ? { color } : null,
+            // areaStyle: getSettingsMap('isArea', seriesIds) ? { color } : null,
         };
     });
 
@@ -165,9 +162,16 @@ const buildOptions = (data: Data[], settings: VisualSettings) => {
 export class Visual implements IVisual {
     private target: HTMLElement;
     private settings: VisualSettings;
+    private metadata: DataViewMetadata;
+    private dataView: DataView;
+    private host: IVisualHost;
+    private data: Data[];
+    private chart: echarts.ECharts;
 
     constructor(options: VisualConstructorOptions) {
         this.target = options.element;
+        this.host = options.host;
+        this.chart = echarts.init(this.target);
     }
 
     public update(options: VisualUpdateOptions) {
@@ -175,22 +179,52 @@ export class Visual implements IVisual {
             options && options.dataViews && options.dataViews[0],
         );
 
-        const dataView: DataView = options.dataViews[0];
+        this.dataView = options.dataViews[0];
+        this.metadata = this.dataView.metadata;
 
-        const data = mapDataView(dataView);
+        this.data = mapDataView(this.dataView);
+        const chartOptions = buildOptions(this.data);
 
-        const chartOptions = buildOptions(data, this.settings);
-
-        console.log(data);
-        console.log(chartOptions);
-        const chart = echarts.init(this.target);
-        chart.setOption(chartOptions);
+        this.chart.resize();
+        this.chart.setOption(chartOptions, true, true);
     }
 
     public enumerateObjectInstances(
         options: EnumerateVisualObjectInstancesOptions,
     ): VisualObjectInstanceEnumeration {
-        const settings = this.settings || VisualSettings.getDefault();
-        return VisualSettings.enumerateObjectInstances(settings, options);
+        let objectName = options.objectName;
+        let objectEnumeration: VisualObjectInstance[] = [];
+
+        if (!this.settings || !this.data) {
+            return objectEnumeration;
+        }
+
+        switch (objectName) {
+            case 'color':
+                zip(
+                    this.metadata.columns.slice(1),
+                    Object.entries(groupBy(this.data, ({ key }) => key)),
+                ).forEach(([{ queryName, objects }, [key]]) => {
+                    objectEnumeration.push({
+                        objectName,
+                        displayName: key,
+                        properties: {
+                            color: {
+                                solid: {
+                                    color: getColor(objects),
+                                },
+                            },
+                        },
+                        propertyInstanceKind: {
+                            color: VisualEnumerationInstanceKinds.Constant,
+                        },
+                        selector: {
+                            metadata: queryName,
+                        },
+                    });
+                });
+                break;
+        }
+        return objectEnumeration;
     }
 }
