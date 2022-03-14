3a8778008c0d3fe42248177c05dffb662c147e5d
HM <hieumdd@gmail.com>
2022-03-14 20:45:19 +0700

    feat: Add tooltip dynamic spacing

--- src/components/tooltip.ts
+++ src/components/tooltip.ts
@@ -8,7 +8,7 @@ const formatter = (seriesValueFormatters: IValueFormatter[]) => (i: any) => {
     tooltipEl.className = 'tooltip';
 
     Object.values(groupBy(i, ({ axisIndex }) => axisIndex))
-        .map((panel) =>
+        .map((panel, _, arr) =>
             panel.map(({ marker, seriesName, value, seriesIndex }: any) => {
                 const seriesEl = document.createElement('div');
                 seriesEl.className = 'tooltip-series';
@@ -30,21 +30,27 @@ const formatter = (seriesValueFormatters: IValueFormatter[]) => (i: any) => {
         )
         .map((seriesEls) => {
             const panelEl = document.createElement('div');
+            panelEl.className = 'tooltip-panel';
             seriesEls.forEach((el) => panelEl.appendChild(el));
             return panelEl;
         })
         .forEach((el) => tooltipEl.appendChild(el));
 
-    return tooltipEl.outerHTML;
+    tooltipEl.style.gap = `${30 / (tooltipEl.childElementCount - 0.5)}em`;
+    return tooltipEl;
 };
 
 const getTooltip = (seriesValueFormatters: IValueFormatter[]) => ({
     trigger: 'axis',
+    className: 'tooltip-container',
+    padding: '0px',
+    backgroundColor: 'rgba(50,50,50,0.0)',
+    extraCssText: 'box-shadow: none;',
     axisPointer: {
         type: 'cross',
     },
     position: (pos, params, el, elRect, size) => ({
-        top: '20%',
+        top: '5%',
         [['left', 'right'][+(pos[0] < size.viewSize[0] / 2)]]: '10%',
     }),
     formatter: formatter(seriesValueFormatters),
