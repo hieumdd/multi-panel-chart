0e9efa3bdb61e2362cf0e2cf273cdbe29026afc2
HM <hieumdd@gmail.com>
2022-12-27 21:47:27 +0700

    feat!: Upgrade API to v5.2.0

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        es2017: true,
+    },
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: '.',
+    },
+    plugins: ['powerbi-visuals'],
+    extends: ['plugin:powerbi-visuals/recommended'],
+    rules: {
+        '@typescript-eslint/no-unused-vars': 0,
+        'powerbi-visuals/no-inner-outer-html': 0,
+    },
+};
